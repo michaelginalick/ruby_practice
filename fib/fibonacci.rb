a48e813a96a637411bd1efcffbaae84f02b6f7d7
Michael <Michael.ginalick@gmail.com>
2015-07-28 19:57:21 -0500

    fib

---
+++ fib/fibonacci.rb
@@ -0,0 +1,26 @@
+def finonacci(n)
+	return 0 if n == 0
+	return 1 if n == 1
+
+		first_prev = 1
+		second_prev = 0
+		sum = 0
+
+		(n-1).times do
+			sum = first_prev + second_prev
+			second_prev = first_prev
+			first_prev = sum
+		end
+		return sum
+end
+
+p finonacci(5)
+
+
+def fin_rec(n)
+	return n if n <= 1
+	fin_rec(n-1) + fin_rec(n-2)
+end
+
+
+p fin_rec(5)
